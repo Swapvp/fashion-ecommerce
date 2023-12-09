242a886ac2250e3564a549d62759dd1c26a4064b
Swapvp <swapneshpawar1994@gmail.com>
2023-12-09 11:09:53 +0530

    mirraw v4

--- cart.js
+++ cart.js
@@ -31,14 +31,49 @@ function displayCartItems() {
 
 function removeFromCart(itemId) {
   cartItems = cartItems.filter((cartItemId) => cartItemId != itemId);
+  if (cartItems.length == 0) {
+    cartEmpty.style.display = "block";
+  }
   localStorage.setItem("cartItems", JSON.stringify(cartItems));
   loadCartItemsObj();
   displayCartItems();
   displayCartCount();
-  cartEmpty.style.display = "block  ";
+}
+
+function formatDate(date) {
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const day = date.getDate();
+  const monthIndex = date.getMonth();
+  const year = date.getFullYear();
+
+  const formattedDate = `${day.toString().padStart(2, "0")} ${
+    months[monthIndex]
+  } ${year}`;
+  return formattedDate;
 }
 
 function generateItemHtml(item) {
+  let currentDate = new Date();
+
+  // Add 5 days to the current date
+  currentDate.setDate(currentDate.getDate() + 5);
+
+  // Format the date
+  let formattedDate = formatDate(currentDate);
   return `<div class="col-lg-8 col-md-12 col-sm-12 cart-bg-color">
             <div class="d-flex cart-info-container">
                 <div class="cart-left-part">
@@ -58,7 +93,7 @@ function generateItemHtml(item) {
                     </div>
                     <div class="delivery-details">
                         Delivery by
-                        <span class="delivery-details-days">10 Oct 2023</span>
+                        <span class="delivery-details-days">${formattedDate}</span>
                     </div>
                 </div>
                 <div class="remove-from-cart" onclick="removeFromCart(${item.id})"><i class="fa-solid fa-trash-can"
